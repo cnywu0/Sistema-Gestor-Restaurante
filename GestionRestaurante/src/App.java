26b5aac71e33b2720b0118154ed0856bca7ef440
Nicolas Blasco <nicolaslol22@gmail.com>
2025-03-17 13:36:10 +0100

    basic class creation, and basic menu

--- GestionRestaurante/src/App.java
+++ GestionRestaurante/src/App.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        int opcion;
+
+        Restaurante restaurante = new Restaurante();
+
+        do {
+            System.out.println("\nBienvenido al Sistema de Restaurante 🍕🍔");
+            System.out.println("1. Registrar mesa");
+            System.out.println("2. Registrar Plato");
+            System.out.println("3. Reservar Pedido");
+            System.out.println("4. Modificar Pedido");
+            System.out.println("5. Borrar Pedido");
+            System.out.println("6. Modificar o Borrar Plato");
+            System.out.println("7. Guardar Datos");
+            System.out.println("8. Cargar Datos");
+            System.out.println("9. Salir");
+            System.out.print("Opcion: ");
+            opcion = sc.nextInt();
+            
+            
+            
+            switch (opcion) {
+                case 1 -> restaurante.registrarMesa();
+                case 2 ->  //registrarPlato();
+                case 3 ->   //reservarPedido();
+                case 4 ->   //modificarPedido();
+                case 5 ->   //borrarPeiddo();
+                case 6 ->   //modificarPlato();
+                case 7 ->   //guardarDatos();
+                case 8 ->   //cargarDatos();
+            }
+            
+        } while (opcion != 9);
     }
 }
